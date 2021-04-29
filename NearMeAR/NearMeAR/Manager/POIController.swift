23d3485542e79d0f355c1dc8a78a58866fe6325d
Marc <marc.daniel.jacques@gmail.com>
2021-04-29 12:07:30 -0400

    Connect to Yelp Fusion API to enrich location information
    - add POIController to manage network calls
    - add several codable data models
    
    The POIController has one public method that performs a URLRequest to the Yelp Fusion API to get business reviews and images.

--- NearMeAR/NearMeAR/Manager/POIController.swift
+++ NearMeAR/NearMeAR/Manager/POIController.swift
@@ -7,58 +7,88 @@
 
 import Foundation
 
-enum NetworkError: Error {
-    case otherError(Error)
-    case noData
-    case decodeFailed
-}
-
 class POIController {
+       //
+    //let baseURL = URL(string: "https://api.yelp.com/v3/businesses/")!
+    ////let baseURL = "https://api.yelp.com/v3/businesses/"
+    let session = URLSession(configuration: .default)
+    var components = URLComponents(string: "https://api.yelp.com/v3/businesses/")!
+    let bearer = "x-2bgd1jRbajaM_rbZHhN50ht25FgohvBiJeGKNx3rFwR-1tT5vOUOdhYsBS9gUR5rZItYvmKyRLo2CbpRDMJOWZa1WaIyU4aD3Esejn6kbPCcGz2yrpel54PrSFYHYx"
+
     
-    var points: [POI] = []
-    
-    private var baseURL = URL(string: "https://api.yelp.com/v3/businesses")!
-    private let bearer = "x-2bgd1jRbajaM_rbZHhN50ht25FgohvBiJeGKNx3rFwR-1tT5vOUOdhYsBS9gUR5rZItYvmKyRLo2CbpRDMJOWZa1WaIyU4aD3Esejn6kbPCcGz2yrpel54PrSFYHYx"
-    
-    func getPOIData(_ place: Place, completion: @escaping (Result<POI, NetworkError>) -> Void) {
-        let url = baseURL.appendingPathComponent("search")
-              
-        var request = URLRequest(url: url)
-        request.setValue("Bearer \(bearer)", forHTTPHeaderField: "Authorization")
+    func fetchBusinessData(place: Place) {
+        components.path = "matches"
+        components.queryItems = place.queryItems
+        
+        let matchURL = components.url!
+        
+        var request = URLRequest(url: matchURL)
+        request.addValue("Bearer \(bearer)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "GET"
         
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                completion(.failure(.otherError(error!)))
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200..<300).contains(httpResponse.statusCode) else { return }
-            
-            guard let data = data else {
-                completion(.failure(.noData))
-                return
-            }
+        
+        session.dataTask(with: request) { data, _, error in
+            guard let data = data else { fatalError() }
+            //        print(response)
             
             do {
-                let point = try JSONDecoder().decode(place.self, from: data)
+                let newBusiness = try JSONDecoder().decode(BusinessList.self, from: data).businesses.first ?? Business(id: "", name: "")
+                print("DEBUG: \(newBusiness.name)")
+                //            business = newBusiness.businesses?.first
+                //            businessDetails.append(newBusiness)
+                let reviews = self.getReviews(business: newBusiness)
+                let photos = self.getImages(business: newBusiness)
+                
+            } catch {
+                print(error)
             }
-        }
-        task.resume()
-    }
-    
-    
-    func getImage() {
-        let imageURL = baseURL
-    }
-    
-    func getReviews(place: Place) {
-        let url = baseURL.appendPathComponent("\(place.id)")
+            
+        }.resume()
         
-        var request = URLRequest(url: url)
-        request.setValue("Bearer \(bearer)", forHTTPHeaderField: "Authorization")
-        request.httpMethod = "GET"
+        //    businesses.append(business)
     }
-    
+
+    private func getReviews(business: Business) -> [Review] {
+        var reviews: [Review] = []
+        components.path = "\(business.id)/reviews"
+        
+        var request = URLRequest(url: components.url!)
+        request.addValue("Bearer \(bearer)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        
+        session.dataTask(with: request) { data, response, error in
+            guard let data = data else { fatalError() }
+            //        print("Debug \(String(describing: response))")
+            
+            do {
+                reviews = try JSONDecoder().decode(Reviews.self, from: data).reviews
+//                print("Debug: \(String(describing: reviews.first?.text))")
+            } catch {
+                print(error)
+            }
+        }.resume()
+        return reviews
+    }
+
+    private func getImages(business: Business) -> [String] {
+        var photos: [String] = []
+        
+        var request = URLRequest(url: components.url!)
+        request.addValue("Bearer \(bearer)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        
+        session.dataTask(with: request) { data, response, error in
+            guard let data = data else { fatalError() }
+            //        print("Debug \(String(describing: response))")
+            
+            do {
+                photos = try JSONDecoder().decode(PointOfInterest.self, from: data).photos
+//                print("Debug: \(String(describing: point.photos.first))")
+            } catch {
+                print(error)
+            }
+        }.resume()
+        return photos
+    }
+
 }
--- NearMeAR/NearMeAR/Model/Place.swift
+++ NearMeAR/NearMeAR/Model/Place.swift
@@ -7,43 +7,78 @@
 
 import Foundation
 
+
 struct Place: Codable {
-    let id: String
     let name: String
-    let rating: Int
-    let photos: [Photo]
-    let location: Location
-    let reviews: [Review]?
-}
-
-struct Photo: Codable {
-    let photoURL: String
-}
-
-struct Location: Codable {
     let address1: String
     let city: String
     let state: String
-    let displayAddress: [Address]
+    let country: String
+    
+    var queryItems: [URLQueryItem] {
+        return [URLQueryItem(name: "name", value: self.name),
+                URLQueryItem(name: "address1", value: self.address1),
+                URLQueryItem(name: "city", value: self.city),
+                URLQueryItem(name: "state", value: self.state),
+                URLQueryItem(name: "country", value: self.country)]
+    }
+}
+
+struct PointOfInterest: Codable {
+    let id: String
+    let name: String
+    let isClosed: Bool
+    let displayPhone: String
+    let rating: Double
+    let location: Address
+    let coordinates: Coordinates
+    let price: String
+    let photos: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case isClosed = "is_closed"
+        case displayPhone = "display_phone"
+        case rating
+        case location
+        case coordinates
+        case price
+        case photos
+    }
+    struct Coordinates: Codable {
+        let latitude: Double
+        let longitude: Double
+    }
     
 }
 
 struct Address: Codable {
-    let address: String
+    let displayAddress: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case displayAddress = "display_address"
+    }
+}
+
+struct Reviews: Codable {
+    let reviews: [Review]
 }
 
 struct Review: Codable {
-    let text: String
+    //        let name: String
     let rating: Int
-    let name: String
+    let text: String
 }
 
-extension Location {
-    enum CodingKeys: String, CodingKey {
-        case address1
-        case city
-        case state
-        case displayAddress = "display_address"
-    }
+
+
+struct BusinessList: Codable {
+    let businesses: [Business]
+}
+
+struct Business: Codable {
+    var id: String
+    var name: String
 }
 
